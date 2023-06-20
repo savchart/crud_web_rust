8d8793a284061ecd680b96231008fb59b4e74c2d
savchart <savchart@fit.cvut.cz>
2023-06-20 19:57:52 +0200

    Creating todos

--- src/db.rs
+++ src/db.rs
@@ -7,6 +7,7 @@ use std::time::Duration;
 use mobc::Pool;
 
 type Result<T> = std::result::Result<T, error::Error>;
+const TABLE_NAME: &str = "todo";
 
 const DB_POOL_MAX_OPEN: u64 = 32;
 const DB_POOL_MAX_IDLE: u64 = 8;
@@ -32,4 +33,27 @@ pub async fn init_db(db_pool: &DBPool) -> Result<()> {
     let con = get_db_con(db_pool).await?;
     con.batch_execute(init_file.as_str()).await.map_err(DBInitError);
     Ok(())
+}
+
+pub async fn create_todo(db_pool: &DBPool, body: TodoRequest) -> Result<Todo>{
+    let con = get_db_con(db_pool).await?;
+    let query = format!("INSERT INTO {} (name) VALUES ($1) RETURNING *", TABLE);
+    let row = con
+        .query_one(query.as_str(), &[&body.name])
+        .await
+        .map_err(DBQueryError)?;
+    Ok(row_to_todo(&row))
+}
+
+fn row_to_todo(row: &Row) ->Todo{
+    let id: i32 = row.get(0);
+    let name: String = row.get(1);
+    let created_at: DateTime<Utc> = row.get(2);
+    let checked: bool = row.get(3);
+    Todo {
+        id,
+        name,
+        created_at,
+        checked,
+    }
 }
